c8e6028b103bb720420f52e0e3ebdedabf1abcd4
agazigi <3486446493@qq.com>
2025-01-08 23:55:01 +0800

    2025.1.8 - git - 8.5.4

--- RNN/RNN.py
+++ RNN/RNN.py
@@ -1,23 +1,33 @@
 import torch as th
 from torch.nn import functional as F
-import utils.Loader as lm
+import utils.Loader as ld
+import math
+import torch.nn as nn
 
 batch_size, num_steps = 32, 35
-train_iter, vocab = lm.load_data_time_machine(batch_size, num_steps)
+train_iter, vocab = ld.load_data_time_machine(batch_size, num_steps)
 
 def try_gpu(i=0):
     return th.device(f'cuda:{i}') if i >= 0 and th.cuda.is_available() else th.device('cpu')
 
 def get_params(vocab_size, num_hiddens, device):
-    num_inputs = num_outputs = vocab_size
+    """
+    初始化 RNN 的模型参数
+    """
+    num_inputs = num_outputs = vocab_size # 输入和输出都是词表的词数 28
 
     def normal(shape):
+        """
+        返回一个参数的初始化
+        """
         return th.randn(size=shape, device=device) * 0.01
 
+    # H_t = ReLU( X_t * W_xh + H_t-1 * W_hh + b_h)
     W_xh = normal((num_inputs, num_hiddens))
     W_hh = normal((num_hiddens, num_hiddens))
     b_h = th.zeros(num_hiddens, device=device)
 
+    # O_t = H_t * W_hq + b_q
     W_hq = normal((num_hiddens, num_outputs))
     b_q = th.zeros(num_outputs, device=device)
 
@@ -27,9 +37,15 @@ def get_params(vocab_size, num_hiddens, device):
     return params
 
 def init_rnn_state(batch_size, num_hiddens, device):
+    """
+    初始化时返回隐状态
+    """
     return (th.zeros((batch_size, num_hiddens), device=device), )
 
-def rnn(inputs, state, params): # 向前传播
+def rnn(inputs, state, params):
+    """
+    向前传播 RNN
+    """
     W_xh, W_hh, b_h, W_hq, b_q = params # 获取参数
     H, = state # 获取隐藏层状态
     outputs = []
@@ -37,9 +53,12 @@ def rnn(inputs, state, params): # 向前传播
         H = th.tanh(th.mm(X, W_xh) + th.mm(H, W_hh) + b_h) # 计算隐藏层状态
         Y = th.mm(H, W_hq) + b_q # 计算输出
         outputs.append(Y) # 保存输出
-    return outputs, (H,) # 返回输出和隐藏层状态
+    return th.cat(outputs, dim=0), (H,) # 返回输出和隐藏层状态
 
 class RNNModelScratch: # RNN模型
+    """
+    RNN 模型
+    """
     def __init__(self, vocab_size, num_hiddens, device, get_params, init_state, forward_fn):
         self.vocab_size= vocab_size
         self.num_hiddens = num_hiddens
@@ -56,6 +75,9 @@ num_hiddens = 512
 net = RNNModelScratch(len(vocab), num_hiddens, try_gpu(), get_params, init_rnn_state, rnn)
 
 def predict(prefix, num_preds, net, vocab, device):
+    """
+    预测
+    """
     state = net.begin_state(batch_size=1, device=device)
     outputs = [vocab[prefix[0]]]
     get_input = lambda: th.tensor([outputs[-1]], device=device).reshape((1, 1))
@@ -67,4 +89,5 @@ def predict(prefix, num_preds, net, vocab, device):
         outputs.append(int(y.argmax(dim=1).reshape(1)))
     return ''.join([vocab.idx_to_token[i] for i in outputs])
 
-predict('time traveller ', 10, net, vocab, try_gpu())
+ans = predict('time traveller ', 10, net, vocab, try_gpu())
+print(ans)
--- RNN/utils/Loader.py
+++ RNN/utils/Loader.py
@@ -15,30 +15,30 @@ import random
 '''
 DATA_HUB = dict() # 数据集存储位置
 DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/' # 数据集URL
-DATA_HUB['time_machine'] = (DATA_URL + 'timemachine.txt', '090b5e7e70c295757f55df93cb0a180b9691891a')
+DATA_HUB['time_machine'] = (DATA_URL + 'timemachine.txt', '090b5e7e70c295757f55df93cb0a180b9691891a') # 后边的是 Hash 值
 
-def download(name, cache_dir=os.path.join('..', 'data')):
-    """Download a file inserted into DATA_HUB, return the local filename.
-
-    Defined in :numref:`sec_kaggle_house`"""
-    assert name in DATA_HUB, f"{name} does not exist in {DATA_HUB}."
-    url, sha1_hash = DATA_HUB[name]
-    os.makedirs(cache_dir, exist_ok=True)
-    fname = os.path.join(cache_dir, url.split('/')[-1])
+def download(name, cache_dir=os.path.join('.', 'data')):
+    """
+    下载数据集
+    """
+    assert name in DATA_HUB, f"{name} does not exist in {DATA_HUB}." # 断言，如果 name 不在 DATA_HUB 中，则报错
+    url, sha1_hash = DATA_HUB[name] # 获取数据集的 URL 和 Hash 值
+    os.makedirs(cache_dir, exist_ok=True) # 创建缓存目录
+    fname = os.path.join(cache_dir, url.split('/')[-1]) # 获取缓存文件的路径
     if os.path.exists(fname):
-        sha1 = hashlib.sha1()
-        with open(fname, 'rb') as f:
+        sha1 = hashlib.sha1() # 初始化哈希对象
+        with open(fname, 'rb') as f: # 打开文件
             while True:
-                data = f.read(1048576)
+                data = f.read(1048576) # 读取 1MB 数据
                 if not data:
                     break
-                sha1.update(data)
-        if sha1.hexdigest() == sha1_hash:
-            return fname  # Hit cache
+                sha1.update(data) # 更新哈希值，就是将 data 加到哈希对象中
+        if sha1.hexdigest() == sha1_hash: # 判断哈希值是否相等
+            return fname # 如果相等，返回文件名
     print(f'Downloading {fname} from {url}...')
-    r = requests.get(url, stream=True, verify=True)
+    r = requests.get(url, stream=True, verify=True) # 发送请求
     with open(fname, 'wb') as f:
-        f.write(r.content)
+        f.write(r.content) # 写入文件
     return fname
 
 
@@ -47,9 +47,12 @@ def download(name, cache_dir=os.path.join('..', 'data')):
     读取数据集，进行正则、小写等
 '''
 def read_time_machine():
+    """
+    读取数据集
+    """
     with open(download('time_machine') ,'r') as f:
-        lines = f.readlines()
-    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+        lines = f.readlines() # 读取所有行
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines] # 正则匹配，去掉非字母，并转换为小写，并且转化为列表
 
 # lines = read_time_machine()
 # print(f'# 文本总行数：{len(lines)}')
@@ -60,10 +63,13 @@ def read_time_machine():
     将内容词元化
 '''
 def tokenize(lines, token='word'):
+    """
+    将整个文本进行词元化
+    """
     if token == 'word':
-        return [line.split() for line in lines]
+        return [line.split() for line in lines] # 将文本拆分为单词
     elif token == 'char':
-        return [list(line) for line in lines]
+        return [list(line) for line in lines] # 将文本拆分为字符
     else:
         print('错误：未知词元类型：' + token)
 
@@ -75,14 +81,23 @@ def tokenize(lines, token='word'):
     制作词表，将字符串的单词和数字相对应
 '''
 def count_corpus(tokens):
-    if len(tokens) == 0 or isinstance(tokens[0], list):
-        tokens = [token for line in tokens for token in line]
-    return collections.Counter(tokens)
+    """
+    统计词频
+    """
+    if len(tokens) == 0 or isinstance(tokens[0], list): # 如果 tokens 是一个列表，则将所有元素合并成一个列表
+        tokens = [token for line in tokens for token in line] # 对 tokens 的每一 line，再对 line 中的每个单词，再对单词进行拆分
+    return collections.Counter(tokens) # 统计词频。返回一个字典，键是单词，值是该单词出现的次数。
 
 class Vocab:
+    """
+    制作词表的类。建立数字索引和单词的映射
+    """
     def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
         """
-        idx_to_token 是存放了单词，给定索引求单词
+        tokens 是整个词元表
+        min_freq 是一个整型，表示词元的最小出现频率，小于这个频率的就不统计
+        reserved_tokens 是一个列表，表示需要保留的单词
+        idx_to_token 这是一个列表，下标就是索引，其中存放的元素是单个词
         token_to_idx 是一个字典，其中单词为键，其值是该单词在 idx_to_token 列表中的索引
         """
         if tokens is None:
@@ -90,13 +105,13 @@ class Vocab:
         if reserved_tokens is None:
             reserved_tokens = [] # 保留的词元初始为空
         counter = count_corpus(tokens) # 统计词频
-        self._token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True) # 按词频排序
-        self.idx_to_token = ['<unk>'] + reserved_tokens # 词表初始化，其中0为unk
+        self._token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True) # 按词频排序。词频从大到小排序
+        self.idx_to_token = ['<unk>'] + reserved_tokens # 词表初始化，其中 0 为 unk
         self.token_to_idx = { # 词表索引初始化
             token: idx for idx, token in enumerate(self.idx_to_token)
         }
-        for token, freq in self._token_freqs:
-            if freq < min_freq:
+        for token, freq in self._token_freqs: # 遍历词频列表
+            if freq < min_freq: # 如果词频小于最小频率，则跳过
                 break
             if token not in self.token_to_idx:
                 self.idx_to_token.append(token)
@@ -120,11 +135,15 @@ class Vocab:
 # print(list(vocab.token_to_idx.items())[:20])
 
 def load_corpus_time_machine(max_tokens=-1):
+    """
+    整合上边的功能，读取数据，词元化，制作词表
+    """
     lines = read_time_machine()
-    tokens = tokenize(lines, 'char')
+    tokens = tokenize(lines, 'char') # 将文本拆分为字符
     vocab = Vocab(tokens)
-    corpus = [vocab[token] for line in tokens for token in line]
-    if max_tokens > 0:
+    corpus = [vocab[token] for line in tokens for token in line] # 对于 tokens 中的 每一 line，再对 line 中的每个单词，再对单词进行拆分
+                                                                # 从而对原文本进行索引化
+    if max_tokens > 0: # 如果最大词数大于 0，则只保留前 max_tokens 个词
         corpus = corpus[:max_tokens]
     return corpus, vocab
 
@@ -146,21 +165,27 @@ def load_corpus_time_machine(max_tokens=-1):
 def seq_data_iter_random(corpus, batch_size, num_steps):
     """
     使用随机抽样生成一个小批量子序列
+    corpus: 对原始文本的索引化
+    batch_size: 批次大小
+    num_steps: 子序列长度
     """
     corpus = corpus[random.randint(0, num_steps-1):] # 从随机偏移量开始对序列进行分区
-    num_subseqs = (len(corpus) - 1) // num_steps #
-    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
-    random.shuffle(initial_indices)
+    num_subseqs = (len(corpus) - 1) // num_steps # 减去 1 是因为我们要考虑标签
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps)) # 获取子序列的索引
+    random.shuffle(initial_indices) # 随机打乱
 
     def data(pos):
+        """
+        给定开始的索引位置，返回长度为 num_steps 的子序列
+        """
         return corpus[pos: pos + num_steps]
 
-    num_batches = num_subseqs // batch_size
+    num_batches = num_subseqs // batch_size # 总的子序列数
     for i in range(0, batch_size * num_batches, batch_size):
-        initial_indices_per_batch = initial_indices[i: i + batch_size]
-        X = [data(j) for j in initial_indices_per_batch]
-        Y = [data(j + 1) for j in initial_indices_per_batch]
-        yield th.tensor(X), th.tensor(Y)
+        initial_indices_per_batch = initial_indices[i: i + batch_size] # 获取子序列的索引
+        X = [data(j) for j in initial_indices_per_batch] # 构造数据
+        Y = [data(j + 1) for j in initial_indices_per_batch] # 构造标签
+        yield th.tensor(X), th.tensor(Y) # 构造小批量
 
 def test1():
     my_seq = list(range(35))
@@ -173,10 +198,10 @@ def test1():
     顺序采样
 '''
 def seq_data_iter_sequential(corpus, batch_size, num_steps):
-    offset = random.randint(0, num_steps)
+    offset = random.randint(0, num_steps) # 随机偏移量
     num_tokens = ((len(corpus) - offset - 1) // batch_size) * batch_size
-    Xs = th.tensor(corpus[offset: offset + num_tokens])
-    Ys = th.tensor(corpus[offset + 1: offset + 1 + num_tokens])
+    Xs = th.tensor(corpus[offset: offset + num_tokens]) # 构造数据
+    Ys = th.tensor(corpus[offset + 1: offset + 1 + num_tokens]) # 构造标签
     Xs, Ys = Xs.reshape((batch_size, -1)), Ys.reshape((batch_size, -1))
     num_batches = Xs.shape[1] // num_steps
     for i in range(0, num_steps * num_batches, num_steps):
@@ -192,6 +217,9 @@ def test2():
 # test2()
 
 class SeqDataLoader:
+    """
+    数据加载器
+    """
     def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
         if use_random_iter:
             self.data_iter_fn = seq_data_iter_random
@@ -203,5 +231,8 @@ class SeqDataLoader:
         return self.data_iter_fn(self.corpus, self.batch_size, self.num_steps)
 
 def load_data_time_machine(batch_size, num_steps, use_random_iter=False, max_tokens=10000):
+    """
+    加载数据，直接用于训练
+    """
     data_iter = SeqDataLoader(batch_size, num_steps, use_random_iter, max_tokens)
     return data_iter, data_iter.vocab
